be4688e84ec78336316eb36945d17a202d95d172
Greg Smith <greg@bocoup.com>
2011-10-13 15:42:53 -0400

    removed usages of __proto__

--- superinherit.js
+++ superinherit.js
@@ -1,25 +1,17 @@
-Object.inherit = (function(protoSupported) {
-    return function(o) {
-        var result;
-        if (o && protoSupported) {
-            o.__proto__ = this;
-            result = o;
-        }
-        else {
-            function F() {}
-            F.prototype = this;
-            var result = new F();
-            if (o) {
-                for (var key in o) {
-                    if (o.hasOwnProperty(key)) {
-                        result[key] = o[key];
-                    }
-                }
+Object.inherit = function(o) {
+    var result;
+    function F() {}
+    F.prototype = this;
+    var result = new F();
+    if (o) {
+        for (var key in o) {
+            if (o.hasOwnProperty(key)) {
+                result[key] = o[key];
             }
         }
-        return result;
     }
-})(Object.__proto__ !== undefined);
+    return result;
+};
 
 Object._super = function _super(_internal, method) {
     var args = [].slice.call(arguments);
@@ -29,8 +21,8 @@ Object._super = function _super(_internal, method) {
         args.unshift(_internal);
     }
     if (_internal.context[method] === _internal.caller && _internal.context.hasOwnProperty(method)) {
-        return _internal.context.__proto__[method].apply(_internal.orig, args.slice(2));
+        return Object.getPrototypeOf(_internal.context)[method].apply(_internal.orig, args.slice(2));
     }
-    args[0].context = _internal.context.__proto__;
+    args[0].context = Object.getPrototypeOf(_internal.context);
     return _super.apply(this, args);
 }
